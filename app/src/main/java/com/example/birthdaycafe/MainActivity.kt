9cf0e519b152caeadb78de4a197a6ace0fa8cb4b
YeSeul-0w0 <choi333ys@gmail.com>
2023-02-25 21:53:33 +0900

    add appbar layout and change toolbar code

--- app/src/main/java/com/example/birthdaycafe/MainActivity.kt
+++ app/src/main/java/com/example/birthdaycafe/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.example.birthdaycafe
 
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.example.birthdaycafe.databinding.ActivityMainBinding
 import java.time.LocalDate
 
 class MainActivity : AppCompatActivity() {
+    var customToolbar: Toolbar? = null
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
@@ -15,8 +17,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        // get month
         val nowDate: LocalDate = LocalDate.now()
         val getMonth: Int = nowDate.monthValue
-        binding.monthTextView.text = "$getMonth"+"월"
+
+        // set toolbar title
+        customToolbar = binding.toolbar
+        setSupportActionBar(customToolbar)
+        getSupportActionBar()?.setTitle("$getMonth"+"월");
+
     }
 }
